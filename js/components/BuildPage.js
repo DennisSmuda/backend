cedb1dcf2e8f8206d9021186cd291c54c96c8a93
Dennis Smuda <dennissmuda@msn.com>
2016-04-14 19:46:55 +0200

    You can now build pages using the three default input fields. The new pages is saved to firebase and can be displayed on the home page

--- js/components/BuildPage.js
+++ js/components/BuildPage.js
@@ -14,10 +14,16 @@ export default class BuildPage extends React.Component {
     this.state =  {
       title: '',
       description: '',
-      paragraph: ''
+      paragraph: '',
     };
+
     this.newPageRef;
+
+    // Bind functions
     this.submit = this.submit.bind(this);
+    this.titleChange = this.titleChange.bind(this);
+    this.descriptionChange = this.descriptionChange.bind(this);
+    this.paragraphChange = this.paragraphChange.bind(this);
   }
 
   componentWillMount() {
@@ -32,9 +38,11 @@ export default class BuildPage extends React.Component {
         <div className="app-body build-page">
           <p>Create a new page</p>
 
-          <input className="title" type="text" placeholder="Title" /><br/>
-          <input className="description" type="text" placeholder="Description" />
-          <textarea className="paragraph" type="text" placeholder="Paragraph" />
+          <form>
+            <input onChange={this.titleChange} value={this.state.title} className="title" type="text" placeholder="Title" /><br/>
+            <input onChange={this.descriptionChange} className="description" type="text" placeholder="Description" />
+            <textarea onChange={this.paragraphChange} className="paragraph" type="text" placeholder="Paragraph" />
+          </form>
 
           <button className="submit" onClick={this.submit}>
             <i className="plus icon"></i>Add page
@@ -44,11 +52,15 @@ export default class BuildPage extends React.Component {
     );
   }
 
+  titleChange(event) { this.setState({title: event.target.value}); }
+  descriptionChange(event) { this.setState({description: event.target.value}); }
+  paragraphChange(event) { this.setState({paragraph: event.target.value}); }
+
   submit() {
-    this.newPageRef = this.pagesRef.child('testpage').set({
-      title: 'Testpage',
-      description: 'test description',
-      paragraph: 'motherfucker'
+    this.newPageRef = this.pagesRef.child(this.state.title).set({
+      title: this.state.title,
+      description: this.state.description,
+      paragraph: this.state.paragraph
     });
   }
 }
